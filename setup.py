ab698b96160b7ed2e58389c5e27fb8a9a2f8edc1
Chansung Park <deep.diver.csp@gmail.com>
2023-04-12 01:33:43 +0000

    add pacakage info

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bingbong',
+    version='0.1',
+    description='Ping pong management library for LLM applied application',
+    author='chansung park',
+    author_email='deep.diver.csp@gmail.com',
+    url='https://github.com/deep-diver/PingPong',
+    install_requires=[],
+    packages=['pingpong'],
+    package_dir={'':'src'},
+    keywords=['LLM', 'pingpong', 'prompt', 'context', 'management'],
+    python_reuqires='>=3.8',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
+    ]
+)
--- src/pingpong/__init__.py
+++ src/pingpong/__init__.py
@@ -1,1 +1,3 @@
+__version__ = '0.1'
+
 from .pingpong import PingPong
