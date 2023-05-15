99d2af606bde6049a5508ab6788f121aa9fff272
chansung <deep.diver.csp@gmail.com>
2023-05-16 04:45:56 +0900

    add starchat

--- src/pingpong/__init__.py
+++ src/pingpong/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.1.21'
+__version__ = '0.1.22'
 
 from .pingpong import PingPong
--- src/pingpong/gradio.py
+++ src/pingpong/gradio.py
@@ -7,6 +7,7 @@ from pingpong.flan import FlanAlpacaChatPPManager
 from pingpong.os_stablelm import OSStableLMChatPPManager
 from pingpong.vicuna import VicunaChatPPManager
 from pingpong.stable_vicuna import StableVicunaChatPPManager
+from pingpong.starchat import StarChatPPManager
 
 class GradioChatUIFmt(UIFmt):
   @classmethod
@@ -108,3 +109,15 @@ class GradioStableVicunaChatPPManager(StableVicunaChatPPManager):
       results.append(fmt.ui(pingpong))
 
     return results    
+  
+class GradioStarChatPPManager(StarChatPPManager):
+  def build_uis(self, from_idx: int=0, to_idx: int=-1, fmt: UIFmt=GradioChatUIFmt):
+    if to_idx == -1 or to_idx >= len(self.pingpongs):
+      to_idx = len(self.pingpongs)
+
+    results = []
+
+    for pingpong in self.pingpongs[from_idx:to_idx]:
+      results.append(fmt.ui(pingpong))
+
+    return results   
